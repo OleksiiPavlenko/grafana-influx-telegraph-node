e5b8942b727d1b90917745383260081883cdbf10
Alexey Pavlenko <alexey.pavlenko@idealscorp.com>
2021-07-19 12:25:40 +0300

    test task

---
+++ app/handlers/things.js
@@ -0,0 +1,30 @@
+// app imports
+const { Thing } = require("../models");
+const { APIError, parseSkipLimit } = require("../helpers");
+const {checkConnection} = require("../connection");
+
+/**
+ * List all the things. Query params ?skip=0&limit=1000 by default
+ */
+async function readThings(request, response, next) {
+  await checkConnection();
+  /* pagination validation */
+  let skip = parseSkipLimit(request.query.skip) || 0;
+  let limit = parseSkipLimit(request.query.limit, 1000) || 1000;
+  if (skip instanceof APIError) {
+    return next(skip);
+  } else if (limit instanceof APIError) {
+    return next(limit);
+  }
+
+  try {
+    const things = await Thing.readThings({}, {}, skip, limit);
+    return response.json(things);
+  } catch (err) {
+    return next(err);
+  }
+}
+
+module.exports = {
+  readThings
+};
